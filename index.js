6bc62950cf45162c6985c1b079d7f024f7889e5f
Deepak Kamat <dkamat.001@gmail.com>
2021-09-22 10:24:58 +0530

    Feature: Date Object Created and Fetched

---
+++ index.js
@@ -0,0 +1,24 @@
+const inputDate = document.querySelector("#input-date");
+const submitBtn = document.querySelector("#submit-btn");
+const outputDiv = document.querySelector("#output");
+
+function getDateObject() {
+  const dateObject = {};
+  const date = inputDate.value.split("-");
+  date.reverse();
+  dateObject.day = date[0];
+  dateObject.month = date[1];
+  dateObject.year = date[2];
+
+  return dateObject;
+}
+
+function getAllDateFormat(dateobj) {
+  let ddmmyyyy = dateobj.day + dateobj.month + dateobj.year;
+  let mmddyyyy = dateobj.month + dateobj.day + dateobj.year;
+  let yyyymmdd = dateobj.year + dateobj.month + dateobj.day;
+  let ddmmyy = dateobj.day + dateobj.month + dateobj.year.slice(-2);
+  let yymmdd = dateobj.year.slice(-2) + dateobj.month + dateobj.day;
+
+  return [ddmmyyyy, mmddyyyy, yyyymmdd, ddmmyy, yymmdd];
+}
